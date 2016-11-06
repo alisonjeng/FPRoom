be8eb156ca409127a064754468ca6bafb9f6fba6
Alison Jeng <alisonjeng95@gmail.com>
2016-11-06 15:35:13 -0800

    fifth room added

---
+++ Assets/Scripts/Doors5.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+	public class Doors5 : MonoBehaviour {
+
+		Animator animator;
+		bool doorOpen;
+
+
+
+		void Start()
+		{
+			doorOpen = false;
+			animator = GetComponent<Animator>();
+		}
+
+		void OnTriggerEnter(Collider col)
+		{
+			if(col.gameObject.tag == "Player" && GameVariables.keyCount>=1)
+			{
+				doorOpen = true;
+				DoorControl ("Open");
+				//GameVariables.keyCount--;
+
+			}
+		}
+
+		void OnTriggerExit(Collider col)
+		{
+			if(doorOpen)
+			{
+				doorOpen = false;
+				DoorControl ("Close");
+				GameVariables.keyCount--; 
+			}
+		}
+
+		void DoorControl(string direction)
+		{
+			animator.SetTrigger(direction);
+		}
+
+	}
